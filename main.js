6d7d262e963719c99994944935f78070d795de97
Rubén <91787030+rubendelaviuda@users.noreply.github.com>
2023-05-08 13:15:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -888,7 +888,7 @@ var app = (function () {
           });
       },
       () => {
-        n("results", "stats"),
+        n("results", "estadísticas"),
           pe("clickStats", {
             name: "clickStats",
           });
@@ -4246,6 +4246,9 @@ var app = (function () {
         <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
     </svg>
 </span> \n                <span class="px-1">¿Te gusta este juego?</span>
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+    </svg>
     <div style="height: 24px; width: 24px; margin-right: 4px;">
 </div>
 `),
