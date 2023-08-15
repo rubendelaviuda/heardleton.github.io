d991685b3fb67db0df807db7b8892298ca0cc6b5
Rubén <91787030+rubendelaviuda@users.noreply.github.com>
2023-08-15 17:38:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3830,7 +3830,7 @@ var app = (function () {
       c() {
         (n = w("div")),
           (n.innerHTML =
-            '<p class="mb-3">Un clon de <a href="https://www.heardle.app/" title="Heardle">Heardle</a>, pero solo con canciones de Reggaetón.</p> \n\n\n\n<p class="text-xs mb-3 text-custom-line">Hecho con <a href="https://developers.soundcloud.com">Soundcloud</a>,\n <a href="https://svelte.dev">Svelte</a>,\n <a href="https://tailwindcss.com">Tailwind</a>,\n <a href="https://fonts.google.com/noto/specimen/Noto+Serif+Display">Noto Serif Display</a>, <a href="https://fonts.google.com/noto/specimen/Noto+Sans">Noto Sans</a>,\n <a href="https://iconsvg.xyz">IconSVG</a>, <a href="https://momentjs.com">momentjs</a>,\n <a href="https://tarekraafat.github.io/autoComplete.js/#/">autocomplete.js</a>, y desarrollado con <a href="https://glitch.com/">Glitch</a>. <br></br>Para Ainhoa <3</p>'),
+            '<p class="mb-3">Un clon de <a href="https://www.heardle.app/" title="Heardle">Heardle</a>, pero solo con canciones de Reggaetón.</p> \n\n\n\n<p class="text-xs mb-3 text-custom-line">Hecho con <a href="https://developers.soundcloud.com">Soundcloud</a>,\n <a href="https://svelte.dev">Svelte</a>,\n <a href="https://tailwindcss.com">Tailwind</a>,\n <a href="https://fonts.google.com/noto/specimen/Noto+Serif+Display">Noto Serif Display</a>, <a href="https://fonts.google.com/noto/specimen/Noto+Sans">Noto Sans</a>,\n <a href="https://iconsvg.xyz">IconSVG</a>, <a href="https://momentjs.com">momentjs</a>,\n <a href="https://tarekraafat.github.io/autoComplete.js/#/">autocomplete.js</a>, y desarrollado con <a href="https://glitch.com/">Glitch</a>. <br></br>Para mi hermana Ainhoa <3</p>'),
           M(n, "class", "text");
       },
       m(e, t) {
@@ -4048,7 +4048,7 @@ var app = (function () {
         c() {
           (n = w("p")),
             (n.innerHTML =
-              '<p class="mb-3">¡Gracias por jugar!</p><p class="mb-3">Si te ha gustado el juego considera seguirme en mis redes sociales.</p>'),
+              '<p class="mb-3">¡Gracias por jugar!</p><p class="mb-3">Si te ha gustado el juego considera seguirme en mis redes sociales.</p><p class="mb-3">- Rubén</p>'),
             (r = x()),
             (s = w("p")),
             (i = x()),
